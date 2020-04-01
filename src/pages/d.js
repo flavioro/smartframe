b8529a38968b0083111f444588e6bd8273bc5df0
flavioro <flavioro@gmail.com>
2020-04-01 12:24:02 -0300

    Frontend javascript, Gatsby, ReactJs, react, react-dom, cenas, contador, controlar remotamente

---
+++ src/pages/d.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const D = function() {
+  return <h1>Mostrando Display</h1>;
+};
+
+export default D;
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <Link to="/d">Ir para D</Link>
+    </div>
+  );
+};
+
+export default Index;
